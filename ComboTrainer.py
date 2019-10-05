ae97795c90c75a5b50419c056398e3c945c3cdc4
cx295 <chadunruh@gmail.com>
2019-10-05 00:05:58 -0500

    Major change to how program execution works. Now it builds a list of strings that give command calls to the actions directly (instead of having to build those commands during their execution). It improved execution reliability but it's still not great.

--- ComboTrainer.py
+++ ComboTrainer.py
@@ -6,7 +6,6 @@
 
 from playsound import playsound
 import keyboard
-import pandas
 import os
 import time
 import json
@@ -25,10 +24,10 @@ from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLay
 #app.exec_()
 
 combo_file_location_path = os.path.join(os.path.pardir, 'Universal_Combo_Trainer', 'combos')
-# combo_data_file_path = os.path.join(combo_file_location_path, 'IAD.json')
-combo_data_file_path = os.path.join(combo_file_location_path, 'dbfz_universal_bnb.json')
+#combo_data_file_path = os.path.join(combo_file_location_path, 'IAD.json')
+#combo_data_file_path = os.path.join(combo_file_location_path, 'dbfz_universal_bnb.json')
 #combo_data_file_path = os.path.join(combo_file_location_path, 'Fireball.json')
-#combo_data_file_path = os.path.join(combo_file_location_path, 'dbfz_vanish_into_2M.json')
+combo_data_file_path = os.path.join(combo_file_location_path, 'dbfz_vanish_into_2M.json')
 
 print(combo_data_file_path)
 
@@ -56,50 +55,39 @@ class Timing:
     def frame_to_seconds(self, frames):
         return frames / 60 * self.scaling
 
-    def wait_frames(self, frames):
-        time.sleep(self.frame_to_seconds(frames))
-
-
-class KeyboardAction:
-    def __init__(self, key_to_press):
-        self.key_to_press = key_to_press
-
-    def getKey(self):
-        return self.key_to_press
-
-    def pressKey(self):
-        keyboard.press(self.key_to_press)
-
-    def releaseKey(self):
-        keyboard.release(self.key_to_press)
-
 
 class TechniqueHandler:
+
     def __init__(self):
+
         pass
 
     def tap(self, keyarray, sound, inwait, afterwait):
+        tapArray = []
         for key in keyarray:
-            keysBound[key].pressKey()
+            tapArray.append("keyboard.press('" + str(keysBound[key]) + "')")
         if sound == 1:
-            play_sound(1)
+            tapArray.append("playsound(sound_file_path)")
         if inwait > 0:
-            t.wait_frames(inwait)
+            tapArray.append("time.sleep(" + str(t.frame_to_seconds(inwait)) + ")")
         for key in keyarray:
-            keysBound[key].releaseKey()
+            tapArray.append("keyboard.release('" + str(keysBound[key]) + "')")
         if afterwait > 0:
-            t.wait_frames(afterwait)
+            tapArray.append("time.sleep(" + str(t.frame_to_seconds(afterwait)) + ")")
+
+        return tapArray
 
 
 class TapHandler(TechniqueHandler):
+
     def __init__(self, combodict):
         self.key = combodict['key']
         self.bip = combodict['bip']
         self.inwait = combodict['in']
         self.afterwait = combodict['aft']
 
-    def do_action(self):
-        self.tap(self.key, int(self.bip), int(self.inwait), int(self.afterwait))
+    def get_action(self):
+        return super().tap(self.key, int(self.bip), int(self.inwait), int(self.afterwait))
 
 
 class SlurHandler(TechniqueHandler):
@@ -112,38 +100,47 @@ class SlurHandler(TechniqueHandler):
         self.afterwait = combodict['aft']
 
     def slur(self, dirs, key, bip, inwait, afterwait):
+        slurArray = []
         for i in range(len(self.dirs) + 1):
             if i < len(self.dirs):
-                keysBound[self.dirs[i]].pressKey()
-                if inwait > 0:
-                    t.wait_frames(inwait)
+                slurArray.append("keyboard.press('" + str(keysBound[self.dirs[i]]) + "')")
+            if inwait > 0:
+                slurArray.append("time.sleep(" + str(t.frame_to_seconds(inwait)) + ")")
             if i > 0 and i != len(dirs):
-                keysBound[dirs[i - 1]].releaseKey()
-        super().tap(self.key, int(self.bip), int(self.inwait), 0)
-        keysBound[dirs[len(dirs) - 1]].releaseKey()
+                slurArray.append("keyboard.release('" + str(keysBound[self.dirs[i-1]]) + "')")
+        slurArray.extend(super().tap(self.key, int(self.bip), int(self.inwait), 0))
+        slurArray.append("keyboard.release('" + str(keysBound[self.dirs[len(dirs) - 1]]) + "')")
 
-    def do_action(self):
-        self.slur(self.dirs, self.key, int(self.bip), int(self.inwait), int(self.afterwait))
+        return slurArray
 
+    def get_action(self):
+        return self.slur(self.dirs, self.key, int(self.bip), int(self.inwait), int(self.afterwait))
 
 class PressHandler(TechniqueHandler):
+
     def __init__(self, combodict):
         self.key = combodict['key']
         self.afterwait = combodict['aft']
 
-    def do_action(self):
-        keysBound[self.key].pressKey()
-        t.wait_frames(int(self.afterwait))
+    def get_action(self):
+        pressArray = []
+        pressArray.append("keyboard.press('" + str(self.key) + "')")
+        pressArray.append("time.wait(" + str(self.afterwait) + ")")
 
+        return pressArray
 
 class ReleaseHandler(TechniqueHandler):
+
     def __init__(self, combodict):
         self.key = combodict['key']
         self.afterwait = combodict['aft']
 
-    def do_action(self):
-        keysBound[self.key].releaseKey()
-        t.wait_frames(int(self.afterwait))
+    def get_action(self):
+        releaseArray = []
+        releaseArray.append("keyboard.release('" + str(self.key) + "')")
+        releaseArray.append("time.wait(" + str(self.afterwait) + ")")
+
+        return releaseArray
 
 
 # get key configuration file and pack it into jsonfile library
@@ -153,12 +150,7 @@ with open(keyconfig_file_path, 'r') as f:
 # build library of command: keypress objects
 keysBound = {}
 for move, letter in jsonfile["buttonConfig"].items():
-    keysBound[move] = KeyboardAction(letter)
-
-
-def play_sound(i):
-    playsound(sound_file_path)
-
+    keysBound[move] = letter
 
 t = Timing(1)
 
@@ -167,26 +159,30 @@ with open(combo_data_file_path, 'r') as f:
     print("combofile is: ", combofile)
     print("length of combofile[combo] is: ", len(combofile["combo"]))
 
-keyboard.wait('esc')
-
 actionsarray = []
 for i in range(len(combofile["combo"])):
     for technique in combofile["combo"][i]:
         if technique == 'tap':
-            print('calling TapHandler')
+            #print('calling TapHandler')
             actionsarray.append(TapHandler(combofile["combo"][i]['tap']))
 
         if technique == 'slur':
-            print('calling SlurHandler')
+            #print('calling SlurHandler')
             actionsarray.append(SlurHandler(combofile["combo"][i]['slur']))
 
         if technique == 'press':
-            print('calling PressHandler')
+            #print('calling PressHandler')
             actionsarray.append(PressHandler(combofile["combo"][i]['press']))
 
         if technique == 'release':
-            print('calling ReleaseHandler')
+            #print('calling ReleaseHandler')
             actionsarray.append(ReleaseHandler(combofile["combo"][i]['release']))
 
+outputScriptArray = []
 for i in actionsarray:
-    i.do_action()
+    outputScriptArray.extend(i.get_action())
+print("ready")
+keyboard.wait('esc')
+
+for j in outputScriptArray:
+    exec(j)
