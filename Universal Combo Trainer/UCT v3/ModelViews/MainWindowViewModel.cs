99f336e5d12aa5659ef62dd8f332f1ff2eaac401
chadunruh <chadunruh@gmail.com>
2019-11-04 20:16:07 -0600

    ActionListHandler model and ResourceFile model added

---
+++ Universal Combo Trainer/UCT v3/ModelViews/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace UCT_v3.ModelViews
+{
+    sealed class MainWindowViewModel : INotifyPropertyChanged
+    {
+    }
+}
---
+++ Universal Combo Trainer/UCT v3/Models/ResourceFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UCT_v3.Models
+{
+    class ResourceFile
+    {
+        public string ResourceFileName { get; set; }
+
+        public string ResourceBasePath { get; set; }
+    }
+}
