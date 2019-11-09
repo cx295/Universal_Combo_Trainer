1fa3c5ea1a1bdb63067ef75a8623552b3958cb15
chadunruh <chadunruh@gmail.com>
2019-11-09 09:04:37 -0600

    More work on implementing MVVM

---
+++ Universal Combo Trainer/UCT v3/ModelViews/BrowseCommand.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace UCT_v3.ModelViews
+{
+    public class BrowseCommand : ICommand
+    {
+        private Action<object> execute;
+
+        private Predicate<object> canExecute;
+
+        private event EventHandler CanExecuteChangedInternal;
+
+        public BrowseCommand(Action<object> execute)
+        {
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+                this.CanExecuteChangedInternal += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+                this.CanExecuteChangedInternal -= value;
+            }
+        }
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute != null && this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            EventHandler handler = this.CanExecuteChangedInternal;
+            if(handler != null)
+            {
+                handler.Invoke(this, EventArgs.Empty);
+            }
+        }
+        public void Destroy()
+        {
+            this.canExecute = _ => false;
+            this.execute = _ => { return; };
+        }
+
+        private static bool DefaultCanExecute(object parameter)
+        {
+            return true;
+        }
+    }
+}
--- Universal Combo Trainer/UCT v3/ModelViews/MainWindowViewModel.cs
+++ Universal Combo Trainer/UCT v3/ModelViews/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using System.Windows.Input;
 
 namespace UCT_v3.ModelViews
 {
@@ -9,6 +10,14 @@ namespace UCT_v3.ModelViews
     {
         private Models.ResourceFile comboresource;
         private Models.ResourceFile keyresource;
+        private ICommand browsecombocommand;
+        private ICommand browsekeycommand;
+
+        public MainWindowViewModel()
+        {
+            comboresource = new Models.ResourceFile();
+            keyresource = new Models.ResourceFile();
+        }
 
         public string ComboPathandFile
         {
